352542a62491437ba314704083d536016b12c49a
yairromero <yair.romero@meltsan.com>
2017-09-15 19:22:06 -0500

    Se acomoda header

--- src/main/java/com/meltsan/pdfcreator/GeneradorReporte.java
+++ src/main/java/com/meltsan/pdfcreator/GeneradorReporte.java
@@ -2,8 +2,11 @@ package com.meltsan.pdfcreator;
 
 import static net.sf.dynamicreports.report.builder.DynamicReports.*;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
@@ -11,10 +14,16 @@ import java.util.Date;
 import javax.imageio.ImageIO;
 
 import net.sf.dynamicreports.report.builder.column.TextColumnBuilder;
+import net.sf.dynamicreports.report.builder.component.ComponentBuilder;
+import net.sf.dynamicreports.report.builder.component.ImageBuilder;
 import net.sf.dynamicreports.report.builder.component.TextFieldBuilder;
 import net.sf.dynamicreports.report.builder.group.ColumnGroupBuilder;
+import net.sf.dynamicreports.report.builder.style.FontBuilder;
 import net.sf.dynamicreports.report.builder.style.StyleBuilder;
 import net.sf.dynamicreports.report.constant.HorizontalTextAlignment;
+import net.sf.dynamicreports.report.constant.ImageScale;
+import net.sf.dynamicreports.report.constant.PageOrientation;
+import net.sf.dynamicreports.report.constant.PageType;
 import net.sf.dynamicreports.report.constant.Rotation;
 import net.sf.dynamicreports.report.constant.VerticalTextAlignment;
 import net.sf.dynamicreports.report.datasource.DRDataSource;
@@ -23,51 +32,49 @@ import net.sf.jasperreports.engine.JRDataSource;
 
 
 public class GeneradorReporte {
-	
-	private StyleBuilder boldCenteredStyle;
 
+	private BufferedImage img = null;
+	ImageBuilder imgHeader = null;
+	ImageBuilder imgFooter = null;
+	File file = null;
 	
-	public GeneradorReporte() {
-		
-	}
-	
-	public void generarReporte() {
-		StyleBuilder boldStyle         = stl.style().bold();
-        StyleBuilder boldCenteredStyle = stl.style(boldStyle).setHorizontalTextAlignment
-                (HorizontalTextAlignment.CENTER);        
-       BufferedImage img = null;
+	public void generarReporte() {     
        
-       try {
+      try {
     	   	ClassLoader classLoader = getClass().getClassLoader();
-    	   	File file = new File(classLoader.getResource("back.png").getFile());
-    	   	img = ImageIO.read(file);
+    	   	file = new File(classLoader.getResource("head.png").getFile());
+    	   	img = ImageIO.read(file);    	
+    	   	imgHeader = cmp.image(img).setImageScale(ImageScale.FILL_FRAME);
+    	   	file = new File(classLoader.getResource("foot.png").getFile());
+    	   	img = ImageIO.read(file);    	
+    	   	imgFooter = cmp.image(img);
        } catch (IOException e) {
        	
        }
        try {
-    	   	report()//create new report design
-                     // .setColumnTitleStyle(boldStyle)
-                     // .setColumnStyle(boldStyle)
-                      .highlightDetailEvenRows()
-          .columns(//add columns
-            col.column(null,"Col_1", type.stringType()),
-            col.column(null,"Col_2", type.stringType())
-           )
-            .summary(
-            		cmp.verticalList()
-                 .add(cmp.text("\n\nHYSTERISIS PLOT").setStyle(boldStyle))
-                 .add(cmp.image(img))
-             )
-              .title(cmp.text("XYZ Hospital").setStyle(boldCenteredStyle))//shows report title
-              .pageFooter(cmp.pageXofY())//shows number of page at page footer
-              .setDataSource(createDataSource())//set datasource
-              .show();//create and show report
+
+    	   //imgBackgroud.setPrintWhenExpression(exp.printInFirstPage());  
+    	   FontBuilder defaultFont = stl.font().setFontName("Arial");  
+    	   defaultFont.setFontSize(30);
+    	  
+    	   StyleBuilder boldStyle = stl.style().bold().setForegroundColor(Color.WHITE);
+    			       			         
+
+    	   	report()
+    	   		.setPageFormat(PageType.A5, PageOrientation.LANDSCAPE)
+    	   		.setTitleBackgroundComponent(imgHeader)
+    	   		.setDefaultFont(defaultFont)
+    	   		.title(cmp.text("Antecedentes").setStyle(boldStyle))    	   		    	   		
+    	   		.addPageFooter(imgFooter)
+            //.toPdf(new FileOutputStream("/Users/Meltsan/Desktop/report.pdf"));
+    	   		.show();
        } catch (DRException e) {
     	   		e.printStackTrace();
        }
+
 	}
 	
-	 private JRDataSource createDataSource() {		
+	 /*private JRDataSource createDataSource() {		
 		       DRDataSource dataSource = new DRDataSource("item", "orderdate", "quantity", "unitprice");		
 		       for (int i = 0; i < 15; i++) {		 
 		          dataSource.add("Book", new Date(), (int) (Math.random() * 10) + 1, new BigDecimal(Math.random() * 100 + 1));		 
@@ -77,6 +84,6 @@ public class GeneradorReporte {
 		          dataSource.add("PDA", new Date(), (int) (Math.random() * 10) + 1, new BigDecimal(Math.random() * 100 + 1));		 
 		       }		 
 		       return dataSource;
-		    }
+		    }*/
 
 }
